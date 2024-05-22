9bc551242011caa885ddb5af1aa828883d740b6a
JoaoPec <joaopecurcino@hotmail.com>
2024-05-21 22:15:33 -0300

    added posts table creation script

--- db/initDB.js
+++ db/initDB.js
@@ -3,7 +3,7 @@ import client from "./pool.js";
 async function createTable() {
 
     try {
-    await client.query(`
+        await client.query(`
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             name VARCHAR(255),
@@ -12,12 +12,32 @@ async function createTable() {
             password VARCHAR(255)
         )
     `);
-    console.log("Table created successfully");
-  } catch (error) {
-    console.log(error);
-  } finally {
-    await client.end();
-  }
+        console.log("Table created successfully");
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.end();
+    }
+}
+
+async function createPostsTable() {
+    try {
+        await client.query(`
+            CREATE TABLE posts (
+             id SERIAL PRIMARY KEY,
+             image_url TEXT NOT NULL,
+             content TEXT NOT NULL,
+             user_id INTEGER NOT NULL,
+             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+             FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+                `);
+
+        console.log("Posts table created successfully");
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.end();
+    }
 }
 
 createTable();
