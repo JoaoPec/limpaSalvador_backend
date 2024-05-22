1c3dea441cc95362a139ff3bf5d4fea635fbdaaf
JoaoPec <joaopecurcino@hotmail.com>
2024-05-21 22:19:36 -0300

    updated createtables script

--- db/initDB.js
+++ db/initDB.js
@@ -1,43 +1,38 @@
 import client from "./pool.js";
 
-async function createTable() {
+async function createTables() {
 
     try {
+
         await client.query(`
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255),
-            phone VARCHAR(255),
-            email VARCHAR(255),
-            password VARCHAR(255)
-        )
-    `);
-        console.log("Table created successfully");
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255),
+                phone VARCHAR(255),
+                email VARCHAR(255),
+                password VARCHAR(255)
+            )
+        `);
+
+
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS posts (
+                id SERIAL PRIMARY KEY,
+                image_url TEXT NOT NULL,
+                content TEXT NOT NULL,
+                user_id INTEGER NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+            )
+        `);
+
+        console.log("Tables created successfully");
     } catch (error) {
-        console.log(error);
-    } finally {
-        await client.end();
+        console.error("Error creating tables:", error);
+    }finally {
+        client.end();
     }
 }
 
-async function createPostsTable() {
-    try {
-        await client.query(`
-            CREATE TABLE posts (
-             id SERIAL PRIMARY KEY,
-             image_url TEXT NOT NULL,
-             content TEXT NOT NULL,
-             user_id INTEGER NOT NULL,
-             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-             FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-                `);
+createTables();
 
-        console.log("Posts table created successfully");
-    } catch (error) {
-        console.log(error);
-    } finally {
-        await client.end();
-    }
-}
-
-createTable();
