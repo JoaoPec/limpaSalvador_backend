12c3b0418f588d2edfb471c36d618beb74ab912e
JoaoPec <joaopecurcino@hotmail.com>
2024-05-26 15:40:48 -0300

    add remote acess to psql db

--- db/pool.js
+++ db/pool.js
@@ -4,12 +4,24 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+//Local database
+
+//const client = new Pool({
+//    user: process.env.DB_USER,
+//    host: process.env.DB_HOST,
+//    database: "limpasalvador",
+//    password: process.env.DB_PASS,
+//    port: process.env.DB_PORT
+//})
+
+// Render database
+
 const client = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: "limpasalvador",
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
+
 
 export default client
